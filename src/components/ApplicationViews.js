312c7a0a9ba1b64c68d2c7c50452a4efc26d169b
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-15 13:32:52 -0500

    Creating top level components for project

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+
+// Welcome goes here
+// Register goes here
+// Login goes here
+// Home goes here
+// Trails goes here
+// Friends goes here
+// Pending Reqs goes here
+
+// TODO: create the first components needed below and import them above
+
+const ApplicationViews = props => {
+    return (
+        <>
+            {/* TODO: will add these routes when ready */}
+            {/* <Route exact path="/welcome" render={props => {
+                return <Home />
+            }}/>
+            <Route exact path="/login" render={props => {
+                return <Login {...props} />
+            }}/>
+            <Route exact path="/register" render={props => {
+                return <Register {...props} />
+            }}/> */}
+            <Route exact path="/home" render={props => {
+                return <Home />
+            }}/>
+            <Route exact path="/trailsearch" render={props => {
+                return <TrailSearch {...props} />
+            }}/>
+            {/* below route will render upon trail search, based on zip code */}
+            {/* <Route path="/trailslist" render={props => {
+                return <TrailsList {...props} />
+            }}/> */}
+            {/* <Route path="/traildetails" render={props => {
+                return <TrailDetails {...props} />
+            }}/> */}
+            <Route exact path="/friends" render={props => {
+                return <FriendsList {...props} />
+            }}/>
+            <Route exact path="/pending" render={props => {
+                return <PendingRequestsList {...props} />
+            }}/>
+        </>
+    )
+}
---
+++ src/mtbconnect.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./components/nav/NavBar";
+import ApplicationViews from "./components/ApplicationViews";
+import "./MtbConnect.css";
+
+const MtbConnect = () => {
+  return (
+    <>
+      <NavBar />
+      <ApplicationViews />
+    </>
+  );
+};
+
+export default MtbConnect;
