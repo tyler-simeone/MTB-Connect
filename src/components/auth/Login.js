bf09c7bf5f3d18357d27bab0666dc807e97b79f2
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-16 16:02:05 -0500

    added some css to Login.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Login.css"
 
 const Login = props => {
   const [credentials, setCredentials] = useState({ email: "" });
@@ -6,18 +7,26 @@ const Login = props => {
   const handleFieldChange = evt => {
     const stateToChange = { ...credentials };
     stateToChange[evt.target.id] = evt.target.value;
-    console.log(stateToChange)
+    console.log(stateToChange);
     setCredentials(stateToChange);
   };
 
   const handleLogin = evt => {
     evt.preventDefault();
-    props.setUser(credentials)
+    props.setUser(credentials);
     props.history.push("/home");
   };
 
   return (
     <>
+      <header className="header">
+        <div className="header-banner-one"></div>
+        <div className="header-banner-two">
+          {/* Insert React Burger here */}
+          <h1 className="text-size--large">MTB Connect</h1>
+          {/* Insert avatar/link here */}
+        </div>
+      </header>
       <form onSubmit={handleLogin} className="loginFormContainer">
         <fieldset>
           <label htmlFor="loginInputField">Email:</label>
