26f47c3c64c98564a3c938456bb86995b8873456
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-16 12:10:57 -0500

    installed react-router-dom and created Home.js component

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,6 +5,7 @@ import { Route, Redirect } from "react-router-dom"
 // Register goes here
 // Login goes here
 // Home goes here
+import Home from "./home/Home"
 // Trails goes here
 // Friends goes here
 // Pending Reqs goes here
@@ -27,9 +28,9 @@ const ApplicationViews = props => {
             <Route exact path="/home" render={props => {
                 return <Home />
             }}/>
-            <Route exact path="/trailsearch" render={props => {
+            {/* <Route exact path="/trailsearch" render={props => {
                 return <TrailSearch {...props} />
-            }}/>
+            }}/> */}
             {/* below route will render upon trail search, based on zip code */}
             {/* <Route path="/trailslist" render={props => {
                 return <TrailsList {...props} />
@@ -37,12 +38,14 @@ const ApplicationViews = props => {
             {/* <Route path="/traildetails" render={props => {
                 return <TrailDetails {...props} />
             }}/> */}
-            <Route exact path="/friends" render={props => {
+            {/* <Route exact path="/friends" render={props => {
                 return <FriendsList {...props} />
-            }}/>
-            <Route exact path="/pending" render={props => {
+            }}/> */}
+            {/* <Route exact path="/pending" render={props => {
                 return <PendingRequestsList {...props} />
-            }}/>
+            }}/> */}
         </>
     )
 }
+
+export default ApplicationViews;
