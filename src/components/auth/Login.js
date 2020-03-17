c1a500e3d76f4be23bfd4e2a29bf33af2da7d494
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-17 11:47:42 -0500

    Updated Login.js to add user login validation

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import "./Login.css"
+import LoginManager from "../../modules/LoginManager";
+import "./Login.css";
 
 const Login = props => {
   const [credentials, setCredentials] = useState({ email: "" });
@@ -7,14 +8,26 @@ const Login = props => {
   const handleFieldChange = evt => {
     const stateToChange = { ...credentials };
     stateToChange[evt.target.id] = evt.target.value;
-    console.log(stateToChange);
     setCredentials(stateToChange);
   };
 
+  // W/o the evt.preventDefault(), if user tries to submit an empty field, the form will still run/submit after the window alert.
   const handleLogin = evt => {
     evt.preventDefault();
-    props.setUser(credentials);
-    props.history.push("/home");
+    if (credentials.email === "") {
+      window.alert("Please enter a valid email address");
+    } else {
+      LoginManager.getAll().then(users => {
+        if (users.find(user => user.email === credentials.email)) {
+          const user = users.find(user => user.email === credentials.email);
+
+          props.setUser(user.id);
+          props.history.push("/home");
+        } else {
+            window.alert("Invalid email")
+        }
+      });
+    }
   };
 
   return (
