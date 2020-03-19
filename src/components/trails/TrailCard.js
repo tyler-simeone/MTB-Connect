66af89bb3e76cd7094e8f6fa8ed0951aeaef27f8
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-19 10:36:33 -0500

    Trail list and Trail card components complete

--- src/components/trails/TrailCard.js
+++ src/components/trails/TrailCard.js
@@ -1,14 +1,17 @@
 import React from "react";
-import "./Trails.css";
+import { Link } from "react-router-dom"
+import "./TrailCard.css";
 
 const TrailCard = props => {
   return (
     <>
       <div className="trailCardContainer">
         <figure className="imageContainer">
-          <img src={require(`${props.trail.img}`)} alt="Trail Image" />
+          {/* <img src={require(`${props.trail.img}`)} alt="Trail Image" /> */}
         </figure>
-        <button onClick={props.history.push("/traildetails")} type="button">Details</button>
+        <Link to="/traildetails">
+          <button type="button">Details</button>
+        </Link>
         <section className="trailCard">
           <h2>{props.trail.name}</h2>
           <p>{props.trail.description}</p>
--- src/components/trails/Trails.js
+++ src/components/trails/Trails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Trails.css";
 import TrailsManager from "../../modules/TrailsManager";
 import TrailCard from "./TrailCard";
