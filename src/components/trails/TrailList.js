1b554fc7a3413f5dd8b07acfe6fb02a8525cb120
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-26 17:44:27 -0500

    playing around w CSS

--- src/components/trails/TrailList.js
+++ src/components/trails/TrailList.js
@@ -4,7 +4,7 @@ import TrailsManager from "../../modules/TrailsManager";
 import TrailCard from "./TrailCard";
 
 const TrailList = props => {
-  const activeUserId = props.activeUserId
+  const activeUserId = props.activeUserId;
   const [zipcode, setZipcode] = useState({ value: "" });
   const [trails, setTrails] = useState([]);
 
@@ -24,35 +24,49 @@ const TrailList = props => {
 
   return (
     <>
-      <form onSubmit={findMatchingTrails} className="trailSearchBox">
-        <input
-          id="value"
-          type="text"
-          onChange={handleFieldChange}
-          placeholder="Enter Zip Code"
-          className="trailSearch"
-        ></input>
-        <button type="submit">Search</button>
+      <div className="trailListContainer">
+        <form onSubmit={findMatchingTrails} className="trailSearchBox">
+          <input
+            id="value"
+            type="text"
+            onChange={handleFieldChange}
+            placeholder="Enter Zip Code"
+            className="trailSearch"
+          ></input>
+          <button type="submit">Search</button>
 
-        <button className="addTrailBtn" onClick={() => props.history.push("/trails/addTrail")}>
-          Add a Trail
-        </button>
-      </form>
+          <button
+            className="addTrailBtn"
+            onClick={() => props.history.push("/trails/addTrail")}
+          >
+            Add a Trail
+          </button>
+        </form>
 
-      {/* Before submit btn clicked and api fetch runs, trails.length is 0, after search its length is > than 0 so will render the cards!! */}
-      {trails.length === 0 ? (
-        <div className="searchDescriptionContainer">
-          <h1 className="searchDescription">Search For Nearby Trails!</h1>
+        <div className="dynamicTrailListContainer">
+          {/* Before submit btn clicked and api fetch runs, trails.length is 0, after search its length is > than 0 so will render the cards!! */}
+          {trails.length === 0 ? (
+            <div className="searchDescriptionContainer">
+              <h1 className="searchDescription">Search For Nearby Trails!</h1>
+            </div>
+          ) : (
+            <div>
+              <section>
+                {trails.map(trail => {
+                  return (
+                    <TrailCard
+                      key={trail.id}
+                      activeUserId={activeUserId}
+                      trail={trail}
+                      {...props}
+                    />
+                  );
+                })}
+              </section>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>
-          <section>
-            {trails.map(trail => {
-              return <TrailCard key={trail.id} activeUserId={activeUserId} trail={trail} {...props} />;
-            })}
-          </section>
-        </div>
-      )}
+      </div>
     </>
   );
 };
