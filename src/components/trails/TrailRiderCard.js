e730d97eb797b05271ee3ae194782abc39677383
Tyler Simeone <tyler.simeone@gmail.com>
2020-03-26 11:32:36 -0500

    Added images for most riders

--- src/components/trails/TrailRiderCard.js
+++ src/components/trails/TrailRiderCard.js
@@ -55,7 +55,7 @@ const TrailRiderCard = props => {
     <>
       <div className="trailRiderCardContainer">
         <div className="riderImageContainer">
-          <img src={`${props.rider.user.avatarImg}`} alt="Trail Rider Image" />
+          <img src={`${props.rider.user.avatarImg}`} width="100" height="100" alt="Trail Rider Image" />
         </div>
         <section className="trailRiderCard">
           <h2>{props.rider.user.fullName}</h2>
