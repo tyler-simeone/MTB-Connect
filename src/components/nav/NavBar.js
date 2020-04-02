fb3357a18355d1a5ec4d0d3be171d5dba30e8466
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-02 14:07:26 -0500

    added styles to all cards

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { withRouter, Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import "./NavBar.css";
+import Typography from "@material-ui/core/Typography";
 
 const NavBar = props => {
   const hasUser = props.hasUser;
@@ -13,42 +14,47 @@ const NavBar = props => {
         <div className="navContainer">
           <nav>
             <Menu disableAutoFocus>
-            <ul>
-              <li>
-                <Link className="menu-item" to="/home">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="menu-item" to="/trails">
-                  Trails
-                </Link>
-              </li>
-              <li>
-                <Link className="menu-item" to="/friends">
-                  Friends
-                </Link>
-              </li>
-              <li>
-                <Link className="menu-item" to="/pending">
-                  Pending Requests
-                </Link>
-              </li>
-              {hasUser ? (
+              <ul>
                 <li>
-                  <Link onClick={clearUser} className="menu-item" to="/welcome">
-                    Logout
+                  <Link className="menu-item" to="/home">
+                    Home
                   </Link>
                 </li>
-              ) : null}
-            </ul>
+                <li>
+                  <Link className="menu-item" to="/trails">
+                    Trails
+                  </Link>
+                </li>
+                <li>
+                  <Link className="menu-item" to="/friends">
+                    Friends
+                  </Link>
+                </li>
+                <li>
+                  <Link className="menu-item" to="/pending">
+                    Pending Requests
+                  </Link>
+                </li>
+                {hasUser ? (
+                  <li>
+                    <Link
+                      onClick={clearUser}
+                      className="menu-item"
+                      to="/welcome"
+                    >
+                      Logout
+                    </Link>
+                  </li>
+                ) : null}
+              </ul>
             </Menu>
           </nav>
         </div>
       </div>
       <div className="headerBannerTwo">
         {/* Insert React Burger here */}
-        <h1 className="text-size--large">MTB Connect</h1>
+        <Typography component="h1" variant="h2">MTB Connect</Typography>
+        {/* <h1 className="text-size--large">MTB Connect</h1> */}
         {/* Insert avatar/link here */}
       </div>
     </header>
