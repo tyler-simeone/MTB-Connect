831711f28ba2b5504035bae4f2d9ecb9159fecdd
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-05 13:23:25 -0500

    working on Material UI dropdown menu

---
+++ src/components/nav/CustomizedMenu.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+// import ListItemLink from "@material-ui/core/ListItemLink";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5"
+  }
+})(props => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center"
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center"
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white
+      }
+    }
+  }
+}))(MenuItem);
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  };
+
+export default function CustomizedMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+      >
+        Open Menu
+      </Button>
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemLink href="/trails">
+            <ListItemText primary="Trails" />
+          </ListItemLink>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <ListItemIcon>
+            <InboxIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </StyledMenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,7 @@ import { withRouter, Link } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import "./NavBar.css";
 import Typography from "@material-ui/core/Typography";
+import CustomizedMenu from "./CustomizedMenu.js"
 
 const NavBar = props => {
   const hasUser = props.hasUser;
@@ -11,49 +12,48 @@ const NavBar = props => {
   return (
     <header className="header">
       <div className="headerBannerOne">
-        <div className="navContainer">
-          <nav>
-            <Menu disableAutoFocus>
-              <ul>
+        <CustomizedMenu />
+      </div>
+      <div className="navMenuContainer">
+        <nav>
+          <Menu disableAutoFocus>
+            <ul>
+              <li>
+                <Link className="menu-item" to="/home">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="menu-item" to="/trails">
+                  Trails
+                </Link>
+              </li>
+              <li>
+                <Link className="menu-item" to="/friends">
+                  Friends
+                </Link>
+              </li>
+              <li>
+                <Link className="menu-item" to="/pending">
+                  Pending Requests
+                </Link>
+              </li>
+              {hasUser ? (
                 <li>
-                  <Link className="menu-item" to="/home">
-                    Home
+                  <Link onClick={clearUser} className="menu-item" to="/welcome">
+                    Logout
                   </Link>
                 </li>
-                <li>
-                  <Link className="menu-item" to="/trails">
-                    Trails
-                  </Link>
-                </li>
-                <li>
-                  <Link className="menu-item" to="/friends">
-                    Friends
-                  </Link>
-                </li>
-                <li>
-                  <Link className="menu-item" to="/pending">
-                    Pending Requests
-                  </Link>
-                </li>
-                {hasUser ? (
-                  <li>
-                    <Link
-                      onClick={clearUser}
-                      className="menu-item"
-                      to="/welcome"
-                    >
-                      Logout
-                    </Link>
-                  </li>
-                ) : null}
-              </ul>
-            </Menu>
-          </nav>
-        </div>
+              ) : null}
+            </ul>
+          </Menu>
+        </nav>
       </div>
       <div className="headerBannerTwo">
         {/* Insert React Burger here */}
-        <Typography component="h1" variant="h2">MTB Connect</Typography>
+        <Typography component="h1" variant="h2">
+          MTB Connect
+        </Typography>
         {/* <h1 className="text-size--large">MTB Connect</h1> */}
         {/* Insert avatar/link here */}
       </div>
