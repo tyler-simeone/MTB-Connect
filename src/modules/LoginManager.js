fcf2de30f6b5b0b43f7156b98c12512eeec4d6b2
Tyler Simeone <tyler.simeone@gmail.com>
2020-08-21 16:42:02 -0500

    removed unnecessary method from LoginManager.js module

--- src/modules/LoginManager.js
+++ src/modules/LoginManager.js
@@ -1,9 +1,6 @@
 const baseURL = "http://127.0.0.1:8000"
 
 export default {
-    // getAll() {
-    //     return fetch(`${baseURL}/login`).then(resp => resp.json())
-    // },
     post(credentials) {
         return fetch(`${baseURL}/login`, {
           method: "POST",
