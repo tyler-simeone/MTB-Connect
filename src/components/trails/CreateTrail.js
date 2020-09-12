60d2363fdf3760529982bac7a3b46ea42a097df1
Tyler Simeone <tyler.simeone@gmail.com>
2020-09-12 14:51:05 -0500

    user can now create a trail and see the pushpin added realtime

--- src/components/trails/CreateTrail.js
+++ src/components/trails/CreateTrail.js
@@ -3,7 +3,7 @@ import "./CreateTrail.css";
 import TrailsManager from "../../modules/TrailsManager";
 
 import { TextField } from "@material-ui/core";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
@@ -34,7 +34,9 @@ const CreateTrail = props => {
 
   const addNewTrail = evt => {
     evt.preventDefault();
+
     setIsLoading(true)
+    
     // will create new trail and then return user to trails page (where they will then have to search for that trail)
     TrailsManager.post(newTrail).then(() => {
       props.history.push("/trails")
