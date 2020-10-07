f441a50e02cdb78a5d72bfff45ed07116e9711c4
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-07 13:10:58 -0500

    testing cors error solution

--- src/modules/LoginManager.js
+++ src/modules/LoginManager.js
@@ -1,11 +1,12 @@
 const baseURL = "http://3.18.215.253:8080";
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 export default {
     getAll() {
-        return fetch(`${baseURL}/users`).then(resp => resp.json())
+        return fetch(`${proxyurl}/${baseURL}/users`).then(resp => resp.json())
     },
     post(credentials) {
-        return fetch(`${baseURL}/login`, {
+        return fetch(`${proxyurl}/${baseURL}/login`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
