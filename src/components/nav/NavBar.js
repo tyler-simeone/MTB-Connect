7bb7f4a17e1db9df70c40673816bf409edfc5ccb
Tyler Simeone <tyler.simeone@gmail.com>
2020-10-20 13:58:32 -0500

    added basic avatar in navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,8 @@ import { withRouter } from "react-router-dom";
 import "./NavBar.css";
 import Typography from "@material-ui/core/Typography";
 import CustomizedMenu from "./CustomizedMenu.js";
+import Avatar from '@material-ui/core/Avatar';
+
 
 const NavBar = props => {
   const hasUser = props.hasUser;
@@ -14,6 +16,10 @@ const NavBar = props => {
         {hasUser ? (
         <>
           <CustomizedMenu hasUser={hasUser} clearUser={clearUser} className="burgerBtn" />
+          {/* Insert avatar/link here */}
+          <Avatar aria-label="profile-avatar" className="avatar">
+              R
+          </Avatar>
         </>
         ) : null}
       </div>
@@ -21,7 +27,6 @@ const NavBar = props => {
         <Typography component="h1" variant="h2">
           MTB Connect
         </Typography>
-        {/* Insert avatar/link here */}
       </div>
     </header>
   );
